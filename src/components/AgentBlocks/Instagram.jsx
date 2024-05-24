a7ffbdc0ba2da34e39265c8e55903850f067a0b1
Ayush Jain <ayushjain2205@gmail.com>
2024-05-24 21:42:15 +0530

    Add link component

--- src/components/AgentBlocks/Instagram.jsx
+++ src/components/AgentBlocks/Instagram.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import AgentBlock from "../Layout/AgentBlock";
 import Select from "../Fields/Select";
 import InputBox from "../Fields/InputBox";
+import Link from "../Fields/Link";
 
 const Instagram = () => {
   const options = [
@@ -14,6 +15,7 @@ const Instagram = () => {
     <AgentBlock icon="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/2048px-Instagram_icon.png">
       <Select placeholder="Select an action" options={options} />
       <InputBox placeholder="no. of images" />
+      <Link />
     </AgentBlock>
   );
 };
