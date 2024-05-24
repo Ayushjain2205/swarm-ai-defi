f0d6624bda6a0b1550ff52e5d98e3d0bca6332ff
Ayush Jain <ayushjain2205@gmail.com>
2024-05-24 21:11:05 +0530

    Update select component for scalability

--- src/components/AgentBlocks/Instagram.jsx
+++ src/components/AgentBlocks/Instagram.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import AgentBlock from "../Layout/AgentBlock";
+import Select from "../Fields/Select";
 
 const Instagram = () => {
+  const options = [
+    { label: "/action", value: "action" },
+    { label: "/comedy", value: "comedy" },
+    { label: "/drama", value: "drama" },
+  ];
+
   return (
     <AgentBlock icon="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/2048px-Instagram_icon.png">
-      hello
+      <Select placeholder="Select an action" options={options} />
     </AgentBlock>
   );
 };
