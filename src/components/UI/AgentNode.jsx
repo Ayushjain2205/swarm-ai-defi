807999d1e66fd099ff52c3dba22f28e3278ecdec
Ayush Jain <ayushjain2205@gmail.com>
2024-05-24 15:42:00 +0530

    Remove plus icon from agent node

--- src/components/UI/AgentNode.jsx
+++ src/components/UI/AgentNode.jsx
@@ -38,26 +38,6 @@ const AgentNode = ({ data }) => {
           isConnectable={false}
         />
       </div>
-      {/* <div
-        className="absolute w-[20px] h-[20px] flex items-center justify-center rounded-full bg-black"
-        style={{ top: "50%", right: "-10px", transform: "translateY(-50%)" }}
-      >
-        <svg
-          width="12"
-          height="12"
-          viewBox="0 0 24 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M12 5v14M5 12h14"
-            stroke="white"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-        </svg>
-      </div> */}
     </>
   );
 };
