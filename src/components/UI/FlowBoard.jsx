62aedbf12625d2227dccc689c6a81ce75061c784
Ayush Jain <ayushjain2205@gmail.com>
2024-05-24 18:15:18 +0530

    Remove dummy nodes

--- src/components/UI/FlowBoard.jsx
+++ src/components/UI/FlowBoard.jsx
@@ -11,49 +11,13 @@ import AgentNode from "./AgentNode";
 
 import "reactflow/dist/style.css";
 
-const initialNodes = [
-  {
-    id: "1",
-    position: { x: 0, y: 0 },
-    data: { label: "1", agent_type: "instagram" },
-    type: "agentNode",
-  },
-  {
-    id: "2",
-    position: { x: 500, y: 200 },
-    data: { label: "2", agent_type: "twitter" },
-    type: "agentNode",
-  },
-  {
-    id: "3",
-    position: { x: 900, y: 0 },
-    data: { label: "3", agent_type: "image" },
-    type: "agentNode",
-  },
-];
-
-const initialEdges = [
-  {
-    id: "e1-2",
-    source: "1",
-    target: "2",
-    type: "smoothstep",
-  },
-  {
-    id: "e2-3",
-    source: "1",
-    target: "3",
-    type: "smoothstep",
-  },
-];
-
 const nodeTypes = { agentNode: AgentNode };
 
 export default function FlowBoard() {
-  const [nodes, setNodes] = useState(initialNodes);
-  const [edges, setEdges] = useState(initialEdges);
+  const [nodes, setNodes] = useState([]);
+  const [edges, setEdges] = useState([]);
   const { project } = useReactFlow();
-  const nodeId = useRef(nodes.length + 1); // Ref to keep track of the node IDs
+  const nodeId = useRef(1); // Ref to keep track of the node IDs
 
   const onNodesChange = useCallback(
     (changes) => setNodes((nds) => applyNodeChanges(changes, nds)),
