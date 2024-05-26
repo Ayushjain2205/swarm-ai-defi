b8eed8af45da0135afc73ba17c43e807f657e3b0
Ayush Jain <ayushjain2205@gmail.com>
2024-05-26 21:25:24 +0530

    Refactor agents node

--- src/components/UI/AgentNode.jsx
+++ src/components/UI/AgentNode.jsx
@@ -2,40 +2,36 @@ import React from "react";
 import { Handle, Position } from "reactflow";
 import * as AgentBlocks from "../AgentBlocks"; // Import all components from AgentBlocks
 
+const AgentComponentsMap = {
+  instagram: AgentBlocks.Instagram,
+  twitter: AgentBlocks.Twitter,
+  image: AgentBlocks.Image,
+  analyse: AgentBlocks.Analyse,
+  report: AgentBlocks.Report,
+  // Add other mappings here
+};
+
 const AgentNode = ({ data }) => {
   const { agent_type } = data;
-
-  // Map agent_type to components
-  const AgentComponentsMap = {
-    instagram: AgentBlocks.Instagram,
-    twitter: AgentBlocks.Twitter,
-    image: AgentBlocks.Image,
-    analyse: AgentBlocks.Analyse,
-    report: AgentBlocks.Report,
-    // Add other mappings here
-  };
-
   const AgentComponent =
     AgentComponentsMap[agent_type] || (() => <div>Unknown Agent Type</div>);
 
   return (
-    <>
-      <div className="relative w-[290px]  bg-white rounded-[10px] border-[2px] border-[#EBEBEB] px-[12px] py-[20px]">
-        <div>
-          <AgentComponent />
-        </div>
-        <Handle
-          type="source"
-          position={Position.Right}
-          style={{ background: "#555" }}
-        />
-        <Handle
-          type="target"
-          position={Position.Left}
-          style={{ background: "#555" }}
-        />
+    <div className="relative w-[290px] bg-white rounded-[10px] border-[2px] border-[#EBEBEB] px-[12px] py-[20px]">
+      <div>
+        <AgentComponent />
       </div>
-    </>
+      <Handle
+        type="source"
+        position={Position.Right}
+        style={{ background: "#555" }}
+      />
+      <Handle
+        type="target"
+        position={Position.Left}
+        style={{ background: "#555" }}
+      />
+    </div>
   );
 };
 
