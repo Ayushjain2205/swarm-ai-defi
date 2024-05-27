3c6cb30a860f96de3fcc9a78d64f50200154b9fb
Ayush Jain <ayushjain2205@gmail.com>
2024-05-28 04:47:04 +0530

    Add board to use page

---
+++ src/components/UI/UseBoard.jsx
@@ -0,0 +1,88 @@
+import React, { useState, useCallback, useRef, useEffect } from "react";
+import { useDrop } from "react-dnd";
+import ReactFlow, {
+  Background,
+  applyEdgeChanges,
+  applyNodeChanges,
+  addEdge,
+  useReactFlow,
+} from "reactflow";
+import AgentNode from "./AgentNode";
+
+import "reactflow/dist/style.css";
+
+const nodeTypes = { agentNode: AgentNode };
+
+export default function UseBoard() {
+  const [nodes, setNodes] = useState([]);
+  const [edges, setEdges] = useState([]);
+  const { project, fitView } = useReactFlow();
+  const nodeId = useRef(1); // Ref to keep track of the node IDs
+
+  const onNodesChange = useCallback(
+    (changes) => setNodes((nds) => applyNodeChanges(changes, nds)),
+    []
+  );
+
+  const onEdgesChange = useCallback(
+    (changes) => setEdges((eds) => applyEdgeChanges(changes, eds)),
+    []
+  );
+
+  const onConnect = useCallback(
+    (params) =>
+      setEdges((eds) => addEdge({ ...params, type: "smoothstep" }, eds)),
+    []
+  );
+
+  const [{ canDrop, isOver }, drop] = useDrop(() => ({
+    accept: "AGENT_BLOCK",
+    drop: (item, monitor) => {
+      const clientOffset = monitor.getClientOffset();
+      const position = project(clientOffset);
+      const newNode = {
+        id: `${nodeId.current++}`, // Generate unique ID using ref
+        position,
+        data: { label: `Node ${nodeId.current - 1}`, agent_type: item.type },
+        type: "agentNode",
+      };
+      setNodes((nds) => [...nds, newNode]); // Append new node to the existing nodes
+    },
+    collect: (monitor) => ({
+      isOver: !!monitor.isOver(),
+      canDrop: !!monitor.canDrop(),
+    }),
+  }));
+
+  return (
+    <div
+      id="flowboard"
+      className="absolute h-[calc(100vh-78px)]"
+      ref={drop}
+      style={{ width: "100%" }}
+    >
+      <ReactFlow
+        nodes={nodes}
+        onNodesChange={onNodesChange}
+        edges={edges}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect} // Handle edge creation
+        nodeTypes={nodeTypes}
+        nodesDraggable
+      >
+        <Background variant="dots" gap={36} size={2} />
+      </ReactFlow>
+      <div className="flex flex-row w-[calc(100vw-96px)] justify-between relative bottom-[75px] px-[30px] z-50">
+        <button
+          className="flex items-center justify-center h-[45px] w-[182px] text-white bg-black rounded-[5px] cursor-pointer disabled:opacity-40"
+          disabled
+        >
+          Preview
+        </button>
+        <button className="flex items-center justify-center h-[45px] w-[182px] text-white bg-black rounded-[5px] cursor-pointer">
+          Publish
+        </button>
+      </div>
+    </div>
+  );
+}
--- src/pages/use.jsx
+++ src/pages/use.jsx
@@ -1,5 +1,17 @@
 import Page from "../components/Layout/Page";
+import { ReactFlowProvider } from "reactflow";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import UseBoard from "../components/UI/UseBoard";
 
 export default function Home() {
-  return <Page>Create</Page>;
+  return (
+    <DndProvider backend={HTML5Backend}>
+      <ReactFlowProvider>
+        <Page>
+          <UseBoard />
+        </Page>
+      </ReactFlowProvider>
+    </DndProvider>
+  );
 }
