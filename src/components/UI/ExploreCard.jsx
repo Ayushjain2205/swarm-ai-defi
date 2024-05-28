38a14ba23712add496a60575faefe68a84025d3a
Ayush Jain <ayushjain2205@gmail.com>
2024-05-28 07:01:11 +0530

    Add workflow image

--- src/components/UI/ExploreCard.jsx
+++ src/components/UI/ExploreCard.jsx
@@ -1,10 +1,26 @@
 import React from "react";
+import { useRouter } from "next/router";
 
-const ExploreCard = ({ title, icon, labels, onClick }) => {
+const ExploreCard = ({ title, icon, labels }) => {
+  const router = useRouter();
+
+  const handleClick = () => {
+    const query = {
+      title,
+      iconSrc: icon.src,
+      iconAlt: icon.alt,
+      labels: JSON.stringify(labels), // Convert labels to a JSON string
+    };
+    router.push({
+      pathname: "/use",
+      query,
+    });
+  };
+
   return (
     <div
       className="flex flex-col gap-[15px] justify-end h-[230px] min-w-[393px] p-[30px] border border-black rounded-[4px] cursor-pointer"
-      onClick={onClick}
+      onClick={handleClick}
     >
       <div className="flex flex-row gap-[14px] items-center">
         <img src={icon.src} className="size-[30px] rounded" alt={icon.alt} />
