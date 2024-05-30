bbc20520edc8f4d9c27fc63d18709c7499e15860
Ayush Jain <ayushjain2205@gmail.com>
2024-05-31 02:32:58 +0530

    Update use input with question input and button

--- src/components/UseBlocks/Browse.jsx
+++ src/components/UseBlocks/Browse.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Loader from "../UI/Loader";
+import UseInput from "../UI/UseInput";
 
 const Browse = () => {
   return (
     <div>
-      <Loader />
+      <Loader isRunning={false} />
+      <UseInput question="which brand should I analyse?" />
     </div>
   );
 };
