6ebbcc97fa0412ec3febb545b03f0bb006eac007
Ayush Jain <ayushjain2205@gmail.com>
2024-05-31 02:44:56 +0530

    Add use thinking

--- src/components/UI/UseThinking.jsx
+++ src/components/UI/UseThinking.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const UseThinking = () => {
-  return <div>UseThinking</div>;
+  return (
+    <div>
+      <p className="text-[18px] font-[500] animate-pulse">Thinking ..</p>
+    </div>
+  );
 };
 
 export default UseThinking;
--- src/components/UseBlocks/Report.jsx
+++ src/components/UseBlocks/Report.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import UseThinking from "../UI/UseThinking";
+import Loader from "../UI/Loader";
 
 const Report = () => {
-  return <div>Report</div>;
+  return (
+    <div>
+      <Loader />
+      <UseThinking />
+    </div>
+  );
 };
 
 export default Report;
