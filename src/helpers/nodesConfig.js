b459b5aea0c8c2d8ef81683270cd7225727b6757
Ayush Jain <ayushjain2205@gmail.com>
2024-05-31 22:23:20 +0530

    Play with node config

--- src/helpers/nodesConfig.js
+++ src/helpers/nodesConfig.js
@@ -5,21 +5,21 @@ const nodesConfig = [
     id: "1",
     type: "useNode",
     data: { use_type: "instagram" },
-    position: { x: 50, y: 200 },
+    position: { x: 50, y: 150 },
     addAfter: 0, // in seconds
   },
   {
     id: "2",
     type: "useNode",
     data: { label: "Second Node", use_type: "browse" },
-    position: { x: 550, y: 200 },
+    position: { x: 50, y: 500 },
     addAfter: 5, // in seconds
   },
   {
     id: "3",
     type: "useNode",
     data: { label: "Second Node", use_type: "browse" },
-    position: { x: 950, y: 200 },
+    position: { x: 550, y: 150 },
     addAfter: 15, // in seconds
   },
   // Add more nodes as needed
