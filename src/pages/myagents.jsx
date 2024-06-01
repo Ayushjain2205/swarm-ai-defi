27ad92af19c7d6eea298b319e3af5554e8bfdff5
Ayush Jain <ayushjain2205@gmail.com>
2024-06-01 12:11:36 +0530

    Basic explore card

--- src/pages/myagents.jsx
+++ src/pages/myagents.jsx
@@ -1,8 +1,25 @@
 import React from "react";
 import Page from "../components/Layout/Page";
+import MyAgentsCard from "../components/UI/MyAgentsCard";
+import cardsData from "../helpers/cardsData";
 
 const myagents = () => {
-  return <Page>myagents</Page>;
+  const cards = cardsData.map((data, index) => (
+    <MyAgentsCard
+      key={index}
+      id={index}
+      {...data}
+      onClick={() => handleCardClick(data)}
+    />
+  ));
+
+  return (
+    <Page>
+      <div className="flex flex-col gap-[40px] px-[30px] py-[40px]">
+        {cards}
+      </div>
+    </Page>
+  );
 };
 
 export default myagents;
