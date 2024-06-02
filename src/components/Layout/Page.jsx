92f1597bffc019bb2b2f8c09d759c934d75d75d7
Ayush Jain <ayushjain2205@gmail.com>
2024-06-02 15:28:12 +0530

    Get font through CDN

--- src/components/Layout/Page.jsx
+++ src/components/Layout/Page.jsx
@@ -1,12 +1,9 @@
 import React from "react";
-import { Work_Sans } from "next/font/google";
 import Navbar from "./Navbar";
-
-const work = Work_Sans({ subsets: ["latin"] });
 
 const Page = ({ children }) => {
   return (
-    <div className={`${work.className}`}>
+    <div>
       <Navbar />
       {children}
     </div>
